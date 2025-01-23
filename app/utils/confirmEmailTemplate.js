36c4381ca3af0bd49c409087e4c4dbbe1de8da7a
Ababukker Siddik <96437368+abubakkerdev@users.noreply.github.com>
2025-01-23 14:59:06 +0600

    url update

--- app/utils/confirmEmailTemplate.js
+++ app/utils/confirmEmailTemplate.js
@@ -1,7 +1,7 @@
 function confirmEmail(info) {
   let TemplateDesign = `
     <h2>Confirm Email Address</h2> 
-    <h4><a href="http://localhost:1010/api/v1/backend/authentication/confirm-email/${info[0]}">Confirm Email</a></h4>
+    <h4><a href="https://secure-backend-6h0m.onrender.com/api/v1/backend/authentication/confirm-email/${info[0]}">Confirm Email</a></h4>
     `;
   return TemplateDesign;
 }
--- app/utils/forgotEmailTemplate.js
+++ app/utils/forgotEmailTemplate.js
@@ -1,7 +1,7 @@
 function forgotEmail(info) {
   let TemplateDesign = `
       <h2>Forgot Password</h2> 
-      <h4><a href="http://localhost:1010/api/v1/backend/authentication/forgot-password/${info[0]}">Change Password</a></h4>
+      <h4><a href="https://secure-backend-6h0m.onrender.com/api/v1/backend/authentication/forgot-password/${info[0]}">Change Password</a></h4>
       `;
   return TemplateDesign;
 }
